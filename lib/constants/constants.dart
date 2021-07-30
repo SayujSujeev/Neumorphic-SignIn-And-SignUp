e44081afd3a0cda315738bd76a5c82781f283e60
SayujSujeev <sayujsujeev550@gmail.com>
2021-07-31 00:44:46 +0530

    initial commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color lightPrimary = Color.fromRGBO(255, 208, 0, 1);
+const Color darkPrimary = Color.fromRGBO(255, 184, 0, 1);
+const Color black = Colors.black;
+const Color darkShadow = Color.fromRGBO(222, 160, 0, 1);
+const Color lightShadow = Color.fromRGBO(255, 208, 0, 1);
+
+const double appPadding = 30;
